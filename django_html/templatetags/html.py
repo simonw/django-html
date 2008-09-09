5c2b90d8a6a4120a445d1de25f8131965f0ee723
Simon Willison <simon@simonwillison.net>
2008-09-09 15:47:46 +0000

    Added the {% field %} tag, which renders a field based on the current doctype and allows extra HTML attributes to be specified

--- django_html/templatetags/html.py
+++ django_html/templatetags/html.py
@@ -1,11 +1,15 @@
 """
 {% doctype "html4" %}
-{% doctype "html4" silent %} <!-- set internal doctype, NOT output it -->
+{% doctype "html4" silent %} # set internal doctype but do NOT output it
 {% doctype "html4trans" %}
 {% doctype "html5" %}
 {% doctype "xhtml1" %}
 {% doctype "xhtml1trans" %}
+
+{% field form.name %} # Outputs correct widget based on current doctype
+{% field form.name class="my-form-class" %} # Adds an attribute
 """
+import re
 
 doctypes = {
   'html4': """<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
@@ -60,3 +64,61 @@ class DoctypeNode(template.Node):
             return doctypes.get(doctype, '')
 
 register.tag('doctype', do_doctype)
+
+xhtml_end_re = re.compile('\s*/>$')
+
+class FieldNode(template.Node):
+    def __init__(self, field_var, extra_attrs):
+        self.field_var = field_var
+        self.extra_attrs = extra_attrs
+    
+    def render(self, context):
+        field = template.resolve_variable(self.field_var, context)
+        # Caling bound_field.as_widget() returns the HTML, but we need to 
+        # intercept this to manipulate the attributes - so we have to 
+        # duplicate the logic from as_widget here.
+        widget = field.field.widget
+        attrs = self.extra_attrs or {}
+        auto_id = field.auto_id
+        if auto_id and 'id' not in attrs and 'id' not in widget.attrs:
+            attrs['id'] = auto_id
+        if not field.form.is_bound:
+            data = field.form.initial.get(field.name, field.field.initial)
+            if callable(data):
+                data = data()
+        else:
+            data = field.data
+        html = widget.render(field.html_name, data, attrs=attrs)
+        # Finally, if we're NOT in xhtml mode ensure no '/>'
+        doctype = getattr(context, '_doctype', 'xhtml1')
+        if doctype in html_doctypes:
+            html = xhtml_end_re.sub('>', html)
+        return html
+
+def do_field(parser, token):
+    # Can't use split_contents here as we need to process 'class="foo"' etc
+    bits = token.contents.split()
+    if len(bits) == 1:
+        raise template.TemplateSyntaxError, \
+            "%r tag takes arguments" % bits[0]
+    field_var = bits[1]
+    extra_attrs = {}
+    if len(bits) > 1:
+        # There are extra name="value" arguments to consume
+        extra_attrs = parse_extra_attrs(' '.join(bits[2:]))
+    return FieldNode(field_var, extra_attrs)
+
+register.tag('field', do_field)
+
+extra_attrs_re = re.compile(r'''([a-zA-Z][0-9a-zA-Z_-]*)="(.*?)"\s*''')
+
+def parse_extra_attrs(contents):
+    """
+    Input should be 'foo="bar" baz="bat"' - output is corresponding dict. 
+    Raises TemplateSyntaxError if something is wrong with the input.
+    """
+    unwanted = extra_attrs_re.sub('', contents)
+    if unwanted.strip():
+        raise template.TemplateSyntaxError, \
+            "Invalid field tag arguments: '%s'" % unwanted.strip()
+    return dict(extra_attrs_re.findall(contents))
