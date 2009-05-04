4010ab912ac55c870616576018b1ce313960cf71
Rob Hudson <rob@cogit8.org>
2009-05-04 21:47:59 +0800

    Adding version info and various files for setup.py.
    Signed-off-by: Simon Willison <simon@simonwillison.net>

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-html',
+    version=__import__('django_html').__version__,
+    description='This package represents an experimental approach to improving the way Django outputs form widgets.',
+    long_description=open('README.rst').read(),
+    author='Simon Willison',
+    author_email='simon@simonwillison.net',
+    url='http://code.google.com/p/django-html/',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
